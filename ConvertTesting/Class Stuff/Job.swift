ea2e3c0df47ce1a46473982946866d800c283f5f
PatrickMcElroy <pdmcelroy1001@gmail.com>
2021-06-08 09:49:14 -0400

    add component array to job struct

--- ConvertTesting/Class Stuff/Job.swift
+++ ConvertTesting/Class Stuff/Job.swift
@@ -15,9 +15,22 @@ struct Job: Hashable, Codable, Identifiable {
     var name: String
     var address: String
     var sortDate: Int = 0
+    var componentList: [Component] = [Component]()
     private enum CodingKeys : String, CodingKey {
             case installationDate = "Installation Date"
             case name = "Opportunity Name"
             case address = "Mailing Address Line 1"
+    }
+    static func == (lhs: Job, rhs: Job) -> Bool {
+        return lhs.id == rhs.id
+    }
+    static func < (lhs: Job, rhs: Job) -> Bool {
+        if (lhs.sortDate < rhs.sortDate) {
+            return true
+        }
+        return false
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
---
+++ ConvertTesting/Class Stuff/LocalData.swift
@@ -0,0 +1,12 @@
+//
+//  LocalData.swift
+//  ConvertTesting
+//
+//  Created by Patrick McElroy on 6/8/21.
+//
+
+import Foundation
+
+class LocalData: ObservableObject {
+    var jobArr: [Job] = newJobs
+}
